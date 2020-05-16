d5238df400a32360c80bf9c787beba9b30895354
sumanmitra19 <mitr.suman99@gmail.com>
2020-05-16 17:21:40 +0530

    First Commit.

---
+++ Integration/src/test/java/FirstPckg/WithGoogle.java
@@ -0,0 +1,21 @@
+package FirstPckg;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WithGoogle {
+
+	@Test
+	public void browserOpening()
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		System.out.println("The page title is: " + driver.getTitle());
+		driver.quit();
+	}
+}
